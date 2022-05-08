191c4476bf027de1a4484d6b8e323ab779c282cf
Utpal Mishra <utpalmishra21official@gmail.com>
2022-05-08 02:26:41 +0100

    Building..

---
+++ slickSlider.js
@@ -0,0 +1,15 @@
+$(document).ready(function (){
+    $(".customer-logos").slick({
+      dots: false,
+      // arrow: false,
+      infinite: true,
+      speed: 700,
+      autoplaySpeed: 700,
+      autoplay: true,
+      slidesToShow: 1,
+      // slidesToScroll: 1,
+      centerMode: true,
+      variableWidth: true
+      // pauseOnHover: true,      
+    });
+});
